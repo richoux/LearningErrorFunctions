acc05801f81c872369d239f8864264de194b3897
Florian Richoux <florian.richoux@polytechnique.edu>
2019-08-14 18:06:54 +0900

    Rename

--- code/eo/compile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ learn_with_genetic_algo.cpp -Iinclude -Llib -leo -leoutils -o learn_with_genetic_algo
---
+++ code/eo/compile_interpolate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ interpolate_with_ga.cpp -Iinclude -Llib -leo -leoutils -o interpolate_with_ga
