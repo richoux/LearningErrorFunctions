b5106be53b662b14a16d7a4104d8a85c5ab104b8
Florian Richoux <florian.richoux@polytechnique.edu>
2019-09-06 10:28:28 +0900

    LATIN option at compilation

--- code/constraints/test/compil.sh
+++ code/constraints/test/compil.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-g++ -I../../latin/ random_draw.cpp ../../latin/latin.cpp -o random_draw -lghost
+if [ "$1" == "LATIN" ]
+then
+		echo "g++ -I../../latin/ -DLATIN random_draw.cpp ../../latin/latin.cpp -o random_draw -lghost"
+		g++ -I../../latin/ -DLATIN random_draw.cpp ../../latin/latin.cpp -o random_draw -lghost
+else
+		echo "g++ -I../../latin/ random_draw.cpp ../../latin/latin.cpp -o random_draw -lghost"
+		g++ -I../../latin/ random_draw.cpp ../../latin/latin.cpp -o random_draw -lghost
+fi
