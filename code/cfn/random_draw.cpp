19a6af1e187bdd7df259709d1fdfd0611f5ffd3d
Florian Richoux <florian.richoux@polytechnique.edu>
2019-09-10 15:56:37 +0900

    Function to make samples in the search space

--- code/cfn/random_draw.cpp
+++ code/cfn/random_draw.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
 
-#include <vector>
 #include <random>
 #include <cmath>
-
-#include <ghost/variable.hpp>
 
 #include "../latin/latin.hpp"
 #include "../utils/randutils.hpp"
 
-using namespace ghost;
-using namespace std;
+#include "random_draw.hpp"
 
 // AllDiff concept 
 #if defined LATIN
@@ -47,35 +43,29 @@ bool alldiff_concept( const vector< Variable >& variables )
 }
 #endif
 
-int main( int argc, char** argv )
+
+vector< vector<int> > random_draw( const vector< Variable >& variables )
 {
-	int nb_vars;
-	if( argc == 1 )
-		nb_vars = 9;
-	else
-	{
-		nb_vars = std::stoi( argv[1] );
-		nb_vars *= nb_vars;
-	}
 
 #if defined LATIN
 	LHS latin;
 #endif
-	
-  // Create variables
-  vector< Variable > variables;
-  for( int i = 0; i < nb_vars; ++i )
-		variables.emplace_back( std::string("v") + std::to_string(i), std::string("v") + std::to_string(i), 0, nb_vars );
 
-  unsigned int counter = 0;
+	unsigned int counter = 0;
   int value;
-  auto domain_size = variables[0].get_domain_size();
-  
+  int domain_size = (int)variables[0].get_domain_size();
+  int nb_vars = (int)variables.size();
+
+#if not defined LATIN
+  vector<int> configuration( nb_vars );
+#endif
+	vector< vector<int> > samples;
+
   unsigned long long int one_percent = static_cast<unsigned long long int>( std::pow( domain_size, nb_vars ) / 100 );
   //cout << "1% -> " << one_percent << "\n";
   
 #if defined LATIN
-  auto latin_draws = latin.sample( variables, gen );
+  auto latin_draws = latin.sample( variables );
 
   for( unsigned long long int i = 0; i < one_percent; i += domain_size )
 #else
@@ -85,22 +75,26 @@ int main( int argc, char** argv )
 #if defined LATIN
 	  for( auto& sample : latin_draws )
 		  if( alldiff_concept( sample ) )
+		  {
 			  ++counter;
+			  samples.push_back( sample );
+		  }
 	  latin_draws = latin.sample( variables, gen );
 #else
 	  for( auto& var : variables )
-	  {
 		  var.pick_random_value();
-		  //cout << value << " ";
-	  }
-	  //cout << "\n";
 
 	  if( alldiff_concept( variables ) )
+	  {
 		  ++counter;
+		  for( int i = 0; i < nb_vars; ++i )
+			  configuration[i] = variables[i].get_value();
+		  samples.push_back( configuration );
+	  }
 #endif
   }
 
-  cout << "Density of solutions: " << ( static_cast<double>(counter) * 100 ) / one_percent << "%\n";
+  // cout << "Density of solutions: " << ( static_cast<double>(counter) * 100 ) / one_percent << "%\n";
   
-  return EXIT_SUCCESS;
+  return samples;
 }
---
+++ code/cfn/random_draw.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <vector>
+#include <ghost/variable.hpp>
+
+using namespace ghost;
+using namespace std;
+
+vector< vector<int> > random_draw( const vector< Variable >& variables );
