61cc3af97519fbc36c20fea38f035808528b2d95
Florian Richoux <florian.richoux@polytechnique.edu>
2019-10-17 06:30:43 +0900

    function_to_learn from learn folder instead of cfn

--- code/sudoku/compile_cppn.sh
+++ code/sudoku/compile_cppn.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -Ofast -W -Wall -Wextra -Wno-sign-compare -Wno-unused-parameter sudoku.cpp ../constraints/all-diff.cpp ../constraints/all-diff_concept.cpp ../constraints/concept.cpp ../cfn/function_to_learn_cppn.cpp -o bin/sudoku -lghost -pthread
+g++ -Ofast -W -Wall -Wextra -Wno-sign-compare -Wno-unused-parameter sudoku.cpp ../constraints/all-diff.cpp ../constraints/all-diff_concept.cpp ../constraints/concept.cpp ../learn/function_to_learn_cppn.cpp -o bin/sudoku -lghost -pthread
