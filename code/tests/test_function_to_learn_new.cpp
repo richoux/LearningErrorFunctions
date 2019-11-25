b4214c41e7a7bc5f63c914e34e2e9ef168051105
Florian Richoux <florian.richoux@polytechnique.edu>
2019-11-25 17:26:25 +0900

    Tests for the Transform/Compare network

---
+++ code/tests/test_function_to_learn_new.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <numeric>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <random>
+
+#include "../learn/function_to_learn_cppn.hpp"
+#include "../utils/convert.hpp"
+
+#include "../utils/randutils.hpp"
+
+using namespace std;
+
+// in function_to_learn_cppn.cpp
+void interpreter_transformation( int number, const vector<double>& inputs, vector<double>& outputs );
+void interpreter_comparison( int number, const vector<double>& inputs, vector<double>& outputs, double parameter );
+void interpreter_comparison( int number, const vector<double>& inputs, vector<double>& outputs, double parameter_1, double parameter_2 );
+void transformation_layer( const vector<double>& inputs, vector<double>& outputs, int nb_vars, int nb_params, const vector<int>& weights );
+int comparison_layer( const vector<double>& inputs, vector<double>& outputs, int nb_vars, int nb_params, const vector<int>& weights, double parameter_1, double parameter_2 );
+double intermediate_g( const vector<int>& weights, const vector<double>& inputs, int nb_vars, int nb_params, double parameter_1, double parameter_2 );
+
+int main()
+{
+	randutils::mt19937_rng rng;
+	
+	vector<double> config( 9 );
+	std::iota( config.begin(), config.end(), 1.0 );
+	cout << "Config: ";
+	std::copy( config.begin(), config.end(), std::ostream_iterator<double>( cout, " " ) );
+
+	// equals + less than
+	vector<int> raw_weights{ 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 };
+
+	auto weights = make_weights( raw_weights );
+	// cout << "\nWeights: ";
+	// std::copy( weights.begin(), weights.end(), std::ostream_iterator<double>( cout, " " ) );
+	
+	// vector<double> outputs_transfo( 9 * number_units_transfo );
+	// transformation_layer( config, outputs_transfo, 9, 1, weights );
+
+	// cout << "\nOutput Transfo: ";
+	// std::copy( outputs_transfo.begin(), outputs_transfo.end(), std::ostream_iterator<double>( cout, " " ) );
+
+	auto cost = g( weights, config, 0, (int)config.size() );
+	cout << "\nCost: " <<  cost << "\n";
+	
+	rng.shuffle(config);
+	cout << "\nConfig: ";
+	std::copy( config.begin(), config.end(), std::ostream_iterator<double>( cout, " " ) );
+	cost = g( weights, config, 0, (int)config.size() );
+	cout << "\nCost: " <<  cost << "\n";
+
+	rng.shuffle(config);
+	cout << "\nConfig: ";
+	std::copy( config.begin(), config.end(), std::ostream_iterator<double>( cout, " " ) );
+	cost = g( weights, config, 0, (int)config.size() );
+	cout << "\nCost: " <<  cost << "\n";
+
+	vector<int> new_config( 9 );
+	rng.generate<std::uniform_int_distribution>( new_config, 1, 9 );
+	vector<double> new_config_d( new_config.begin(), new_config.end() );	
+	
+	cout << "\nNew config: ";
+	std::copy( new_config_d.begin(), new_config_d.end(), std::ostream_iterator<double>( cout, " " ) );
+	cost = g( weights, new_config_d, 0, (int)new_config_d.size() );
+	cout << "\nCost: " <<  cost << "\n";
+
+	rng.shuffle(new_config_d);
+	cout << "\nNew config: ";
+	std::copy( new_config_d.begin(), new_config_d.end(), std::ostream_iterator<double>( cout, " " ) );
+	cost = g( weights, new_config_d, 0, (int)new_config_d.size() );
+	cout << "\nCost: " <<  cost << "\n";
+
+	return EXIT_SUCCESS;
+}
