cf15f302b1a9c6a35e056c569de319144cd8b692
Florian Richoux <florian.richoux@polytechnique.edu>
2019-11-25 17:26:02 +0900

    Change AllDiff for the Transform/Compare network

--- code/problems/sudoku/compile_sudoku.sh
+++ code/problems/sudoku/compile_sudoku.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -Ofast -W -Wall -Wextra -Wno-sign-compare -Wno-unused-parameter sudoku.cpp ../../constraints/all-diff.cpp ../../constraints/all-diff_concept.cpp ../../constraints/concept.cpp ../../learn/function_to_learn_cppn.cpp -o bin/sudoku -lghost -pthread
+g++ -O3 -W -Wall -Wextra -Wno-sign-compare -Wno-unused-parameter sudoku.cpp ../../constraints/all-diff.cpp ../../constraints/all-diff_concept.cpp ../../constraints/concept.cpp ../../learn/function_to_learn_cppn.cpp ../../utils/convert.cpp -o bin/sudoku -lghost -pthread
