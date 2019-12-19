48ced135b7c74185a46c570d26c13d278b3ea763
Florian Richoux <florian.richoux@polytechnique.edu>
2019-12-19 16:01:19 +0900

    With files this time

---
+++ code/learn/create_complete_spaces.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+./bin/make_complete_spaces -c ad -n 6 -d 6 -o spaces/complete_ad-6-6.txt
+./bin/make_complete_spaces -c lt -n 4 -d 7 -o spaces/complete_lt-4-7.txt
+./bin/make_complete_spaces -c le -n 4 -d 7 -p 16 -o spaces/complete_le-4-7-16.txt
+./bin/make_complete_spaces -c ol -n 3 -d 9 -p 2 -o spaces/complete_ol-3-9-2.txt
+./bin/make_complete_spaces -c cm -n 6 -d 6 -o spaces/complete_cm-6-6-2.txt
