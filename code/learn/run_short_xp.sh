b2389bb479135dc2b1bf1eb64e0b13b9c6b49fb9
Florian Richoux <florian.richoux@polytechnique.edu>
2019-12-28 16:19:37 +0900

    New complete small spaces

--- code/learn/run_short_xp.sh
+++ code/learn/run_short_xp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in {1..100}; do
-		./bin/learn_cf_ga -c ol -n 3 -d 8 -p 2 -ci spaces/complete_ol-3-8-2.txt --xp >> ./results/complete-ol-3-8-2.txt
+		./bin/learn_cf_ga -c ad -n 4 -d 5 -ci spaces/complete_ad-4-5.txt --xp >> ./results/complete-ad-4-5.txt
 done
