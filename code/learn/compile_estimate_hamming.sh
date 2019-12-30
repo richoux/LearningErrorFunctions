e302c90f4f0fec01b0c1618eed4c0be88fb4fe4d
Florian Richoux <florian.richoux@polytechnique.edu>
2019-12-30 05:22:18 +0100

    Give include path for Argh

--- code/learn/compile_estimate_hamming.sh
+++ code/learn/compile_estimate_hamming.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -O3 estimate_hamming.cpp ../utils/metrics.cpp ../utils/convert.cpp -o bin/estimate_hamming
+g++ -O3 estimate_hamming.cpp ../utils/metrics.cpp ../utils/convert.cpp -Iinclude -o bin/estimate_hamming
