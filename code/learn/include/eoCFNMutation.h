2fb66c7b1d6152f326fc073b9718cd5988848887
Florian Richoux <florian.richoux@polytechnique.edu>
2020-01-02 19:46:45 +0900

    Mutate one bit only, and check constraint

--- code/learn/include/eoCFNMutation.h
+++ code/learn/include/eoCFNMutation.h
@@ -37,11 +37,9 @@ public:
 	/**
 	 * Ctor - no requirement
 	 */
-	eoCFNMutation( int number_units_transfo, int number_units_compar, const double& rate = 0.01, bool normalize = false )
+	eoCFNMutation( int number_units_transfo, int number_units_compar )
 		: _number_units_transfo( number_units_transfo ),
-		  _number_units_compar( number_units_compar ),
-		  _rate( rate ),
-		  _normalize( normalize )
+		  _number_units_compar( number_units_compar )
 	{}
 
 	/// The class name. Used to display statistics
@@ -53,7 +51,6 @@ public:
 	 */
 	bool operator()( GenotypeT& genotype )
 	{
-		double actualRate = _normalize ? _rate / genotype.size() : _rate;
 		bool changed_something = false;
 		auto active_transfo_units = std::count( genotype.begin(), genotype.begin() + _number_units_transfo, true );
 		auto active_compar_units = std::count( std::prev( genotype.end(), _number_units_compar ), genotype.end(), true );
@@ -63,18 +60,23 @@ public:
 		std::copy( genotype.begin(), genotype.end(), std::ostream_iterator<int>( cout, " " ) );
 		cout << "\n";
 #endif
-		
-		for( int i = 0; i < (int)genotype.size(); ++i )
-			// Forbid flipping the unique active unit in the transformation layer. 
-			if( eo::rng.flip( actualRate ) && !( i < _number_units_transfo && active_transfo_units == 1 && genotype[i] ) )
-			{
-				// If we are about to flip a comparison unit, switch all of them to false to have a unique active unit in this layer.
-				if( i >= (int)genotype.size() - _number_units_compar )
-					std::fill( std::prev( genotype.end(), _number_units_compar ), genotype.end(), false );
-				genotype[i] = !genotype[i];
-				changed_something = true;
-			}
+	
+		unsigned i;
 
+		// Mutate one bit only
+		// Forbid flipping the unique active unit in the transformation layer. 
+
+		do
+		{
+			i = eo::rng.random( genotype.size() );
+		} while( i < _number_units_transfo && active_transfo_units == 1 && genotype[i] );
+		
+		// If we are about to flip a comparison unit, switch all of them to false to have a unique active unit in this layer.
+		if( i >= (int)genotype.size() - _number_units_compar )
+			std::fill( std::prev( genotype.end(), _number_units_compar ), genotype.end(), false );
+		genotype[i] = !genotype[i];
+		changed_something = true;
+		
 #if defined DEBUG
 		if( changed_something )
 		{
