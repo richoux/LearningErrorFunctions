c8ae22ae92ca6abcde6aba8ff04729ff825627ef
Florian Richoux <florian.richoux@polytechnique.edu>
2021-10-15 21:42:52 +0900

    Update compilation scripts

--- problems/sudoku/scripts/compile_sudoku_hardcoded.sh
+++ problems/sudoku/scripts/compile_sudoku_hardcoded.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -std=c++17 -DHARDCODED -DGHOST_BENCH -O3 -W -Wall -Wextra -Wno-sign-compare -Wno-unused-parameter -o ../bin/sudoku_hardcoded ../../main.cpp ../src/builder.cpp ../src/user_print.cpp ../../../constraints/all-diff_hardcoded.cpp ../../../constraints/all-diff_concept.cpp ../../../constraints/concept.cpp -I../../../learn/include -I../src -L../../../learn/lib -lghost_static -pthread
+g++ -std=c++17 -DHARDCODED -DGHOST_BENCH -O3 -W -Wall -Wextra -Wno-sign-compare -Wno-unused-parameter -o ../bin/sudoku_hardcoded ../../main.cpp ../src/builder.cpp ../src/user_print.cpp ../../../constraints/all-diff_hardcoded.cpp ../../../constraints/all-diff_concept.cpp ../../../constraints/concept.cpp -I../../../learn_ga/include -I../src -L../../../learn_ga/lib -lghost_static -pthread
